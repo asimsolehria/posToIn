92e64182307d9f69708f84b08f5c78a3939b1a4d
Asim Habib <de.asimhabib@gmail.com>
2020-12-22 00:05:16 +0500

    wrote a method to convert a postfix to infix expression using stack

--- src/postoin/PosToIn.java
+++ src/postoin/PosToIn.java
@@ -1,12 +1,32 @@
 
 package postoin;
 
+import java.util.Stack;
+
 /**
  *
  * @author asim
  */
 public class PosToIn 
 {
-
-    
+    public static String posToIn(String exp)
+    {
+        Stack<String> stack=new Stack<>();
+        
+        for (int i = 0; i < exp.length(); i++) 
+        {
+            char c=exp.charAt(i);
+            if(Character.isLetterOrDigit(c))
+                stack.push(c+"");
+            else
+            {
+                String opnd1=stack.pop();
+                String opnd2=stack.pop();
+                String infix="("+opnd2+c+opnd1+")";
+                stack.push(infix);
+            }
+            
+        }
+        return stack.pop();
+    }
 }
---
+++ src/postoin/main.java
@@ -0,0 +1,16 @@
+
+package postoin;
+
+/**
+ *
+ * @author asim
+ */
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        String postFix="ABC/-AK/L-*";
+        
+        System.out.println(PosToIn.posToIn(postFix));
+    }
+}
